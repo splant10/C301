8adab6821e1f51ee9ce35a06ac4f94f3058de9a4
splant10 <splant.10@gmail.com>
2015-10-03 16:32:55 -0600

    Made alert function to alert who buzzes first

--- ReactionGameshowBuzzer2000/app/src/main/java/ca/ualberta/splant/reactiongameshowbuzzer2000/GameShowActivity.java
+++ ReactionGameshowBuzzer2000/app/src/main/java/ca/ualberta/splant/reactiongameshowbuzzer2000/GameShowActivity.java
@@ -1,12 +1,16 @@
 package ca.ualberta.splant.reactiongameshowbuzzer2000;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class GameShowActivity extends AppCompatActivity {
+public class GameShowActivity extends MainScreen {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +39,68 @@ public class GameShowActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void twoPlayerClick(View view) {
+        Intent intent= new Intent(this,TwoPlayerBuzzer.class);
+        startActivity(intent);
+    }
+
+    public void threePlayerClick(View view) {
+        Intent intent= new Intent(this,ThreePlayerBuzzer.class);
+        startActivity(intent);
+    }
+
+    public void fourPlayerClick(View view) {
+        Intent intent= new Intent(this,FourPlayerBuzzer.class);
+        startActivity(intent);
+    }
+
+    // Alert function to display who buzzed first
+    // Source: David Hedlund @ stackoverflow:
+    // http://stackoverflow.com/questions/2115758/how-to-display-alert-dialog-in-android
+    public void alertWhoBuzzed(String name, int count) {
+        new AlertDialog.Builder(this)
+                .setMessage(name + " was the first to buzz! That's " + count + "clicks for " + name + "!")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue playing game
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // return to main screen
+                        finish();
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        startActivity(intent);
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    public void player1Click() {
+        this.player1.incrementBuzzClicks();
+        // Alert that player1 buzzed first
+        alertWhoBuzzed(player1.getName(),player1.getBuzzClicks());
+    }
+
+    public void player2Click() {
+        this.player2.incrementBuzzClicks();
+        // Alert that player2 buzzed first
+        alertWhoBuzzed(player2.getName(),player2.getBuzzClicks());
+    }
+
+    public void player3Click() {
+        this.player3.incrementBuzzClicks();
+        // Alert that player3 buzzed first
+        alertWhoBuzzed(player3.getName(),player3.getBuzzClicks());
+    }
+
+    public void player4Click() {
+        this.player4.incrementBuzzClicks();
+        // Alert that player4 buzzed first
+        alertWhoBuzzed(player4.getName(),player4.getBuzzClicks());
+    }
+
 }
--- ReactionGameshowBuzzer2000/app/src/main/java/ca/ualberta/splant/reactiongameshowbuzzer2000/Player.java
+++ ReactionGameshowBuzzer2000/app/src/main/java/ca/ualberta/splant/reactiongameshowbuzzer2000/Player.java
@@ -5,27 +5,36 @@ package ca.ualberta.splant.reactiongameshowbuzzer2000;
  */
 public class Player {
 
-    private int _buzzClicks;
+    private int buzzClicks;
+    private int reactionTime;
     private String name;
 
-
-    public Player () {
-        _buzzClicks = 0;
+    public Player (String name) {
+        buzzClicks = 0;
+        this.name = name;
     }
 
     public int getBuzzClicks() {
-        return _buzzClicks;
+        return buzzClicks;
     }
 
-    public void setBuzzClicks(int clicks) {
-        this._buzzClicks = clicks;
+    public void incrementBuzzClicks() { this.buzzClicks += 1; }
+
+    public void clearBuzzClicks() { this.buzzClicks = 0; }
+
+    public int getReactionTime() {
+        return reactionTime;
+    }
+
+    public void setReactionTime(int reactionTime) {
+        this.reactionTime = reactionTime;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Player getPlayer() {
+        return this;
     }
 }
